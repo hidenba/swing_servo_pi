0ca39cab02bf8b364d744637692595d2fa7b13b2
HIDEKUNI Kajita <hide.nba@gmail.com>
2015-08-27 02:20:53 +0900

    cosmetic

--- lib/swing_servo_pi.rb
+++ lib/swing_servo_pi.rb
@@ -5,8 +5,9 @@ module SwingServoPi
   class Servo
     attr_reader :gpio, :yaw_pin, :pitch_pin
 
-    INITIAL_POSITION = 15
+    INITIAL_POSITION = 0
     RANGE = (-10..10)
+    OFFSET = 15
 
     def initialize(yaw_pin, pitch_pin)
       @yaw_pin = yaw_pin
@@ -18,11 +19,11 @@ module SwingServoPi
     end
 
     def yawing(position)
-      swing(yaw_pin, adjust_position(position))
+      swing(yaw_pin, position)
     end
 
     def pitching(position)
-      swing(pitch_pin, adjust_position(position))
+      swing(pitch_pin, position)
     end
 
     def reset
@@ -41,13 +42,13 @@ module SwingServoPi
 
     def pwm_init(pin)
       gpio.pin_mode(pin, WiringPi::PWM_OUTPUT)
-      gpio.soft_pwm_create(pin, INITIAL_POSITION, 100)
+      gpio.soft_pwm_create(pin, adjust_position(INITIAL_POSITION), 100)
     end
 
     def swing(pin, position)
-      gpio.soft_pwm_write(pin, position)
-      sleep 0.05
-      io.soft_pwm_write(pin, 0)
+      gpio.soft_pwm_write(pin, adjust_position(position))
+      sleep 1
+      gpio.soft_pwm_write(pin, 0)
     end
   end
 end
